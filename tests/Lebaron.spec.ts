0e50d9a408d3c2c55dfe098607f2e474ceed8fbd
zach <perry.z@husky.neu.edu>
2025-07-12 18:55:04 -0400

    Refactored

--- tests/Lebaron.spec.ts
+++ tests/Lebaron.spec.ts
@@ -1,68 +1,20 @@
 import { test as setup, expect } from '@playwright/test';
-import fs from 'fs-extra'
 import 'dotenv/config';
+import { emptyDirectory, formatDate, removecarts, finalize, confirmOrRetry, whatday, formatDay} from './aux-functions';
+
+emptyDirectory('./screenshots');
 
 //Variables for the Boys
+declare var process : {
+  env: {
+      USERNAME: string;
+      PASSWORD: string;
+      URL: string;
+  }
+}
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
 const WeekendTeeTimes = ['9:15am', '9:24am', '9:33am']
-
-
-// Function to delete all files within a directory
-async function emptyDirectory(directory) {
-  try {
-    fs.emptyDir(directory);
-    console.log(`Successfully emptied directory: ${directory}`);
-  } catch (err) {
-    console.error(`Error emptying directory: ${directory}`, err);
-  }
-}
-emptyDirectory('./screenshots');
-
-// Custom Date Function
-function formatDate(date: Date) {
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  const year = date.getFullYear();
-
-  return `${month}-${day}-${year}`;
-}
-
-function formatDay(date: Date) {
-  const day = String(date.getDate()).padStart(2, '0');
-  return `${day}`;
-}
-
-function whatday(date: Date) {
-  const day = date.getDay();
-  return `${day}`;
-}
-
-async function confirmOrRetry(page,SecondTime) {
-    var count = await page.getByText('This tee time is not available.').count();
-    if (count > 0) {
-      console.log('Tee Time not available');
-      await page.getByText('Ok').first().click();
-      await page.getByText(SecondTime).first().click();
-    } else {
-      console.log('available');
-    }
-  }
-
-async function removecarts(page) {
-  await page.waitForTimeout(3000);
-  const cartElements = await page.getByText('Cart').all();
-  var x = 0
-  while (x < cartElements.length) {
-    await page.getByText('Cart').nth(x).click();
-    x += 1;
-  }
-}
-async function finalize(page) {
-  await page.waitForTimeout(3000);
-  await page.getByText('Continue').first().click();
-  await page.getByText('Finalize Reservation').first().click();
-}
 
 // Get current date and create a new date that is 7 days ahead
 const currentDate = new Date();
@@ -85,7 +37,7 @@ if (Day[0] == '0') {
 }
 console.log(Day)
 
-////////////////////////////////////////START OF TESTS//////////////////////////////////////////////////////
+////////////////////////////////////////START OF FlOW//////////////////////////////////////////////////////
 setup('Check for Tee Times', async ({ page }) => {
 
   await page.goto('https://lebaronhills.cps.golf/onlineresweb/auth/verify-email');
@@ -96,7 +48,7 @@ setup('Check for Tee Times', async ({ page }) => {
   await page.locator('input[name="password"]').fill(password);
   await page.locator('button').filter({ hasText: 'Sign In' }).nth(1).click();
 
-  //Get Tee Time, Open Box
+  
   const DayofWeek = whatday(currentDate)
   console.log(DayofWeek)
 
@@ -104,15 +56,10 @@ setup('Check for Tee Times', async ({ page }) => {
   if (DayofWeek == '1') {
     console.log('It is a Monday, No Tee Times')
     await page.getByText(Day, { exact: true }).click();
-    //await page.getByText('6:20pm').first().click();
-    //await removecarts(page)
-    //await finalize(page)
   }
 
   //Tuesday
   if (DayofWeek == '2') {
-    //await page.getByText(Day, { exact: true }).first().click();
-    //await page.locator('button:visible').getByText(Day, { exact: true }).first().click();
     await page.getByText(Day, { exact: true }).click();
     await page.getByText('Show more Mid Day tee times').first().click();
     await page.getByText('12:30pm').first().click();
---
+++ tests/aux-functions.ts
@@ -0,0 +1,56 @@
+import fs from 'fs-extra'
+
+// Function to delete all files within a directory
+export async function emptyDirectory(directory) {
+  try {
+    fs.emptyDir(directory);
+    console.log(`Successfully emptied directory: ${directory}`);
+  } catch (err) {
+    console.error(`Error emptying directory: ${directory}`, err);
+  }
+}
+
+// Custom Date Function
+export function formatDate(date: Date) {
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const year = date.getFullYear();
+
+  return `${month}-${day}-${year}`;
+}
+
+export function formatDay(date: Date) {
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${day}`;
+}
+
+export function whatday(date: Date) {
+  const day = date.getDay();
+  return `${day}`;
+}
+
+export async function confirmOrRetry(page,SecondTime) {
+    var count = await page.getByText('This tee time is not available.').count();
+    if (count > 0) {
+      console.log('Tee Time not available');
+      await page.getByText('Ok').first().click();
+      await page.getByText(SecondTime).first().click();
+    } else {
+      console.log('available');
+    }
+  }
+
+export async function removecarts(page) {
+  await page.waitForTimeout(3000);
+  const cartElements = await page.getByText('Cart').all();
+  var x = 0
+  while (x < cartElements.length) {
+    await page.getByText('Cart').nth(x).click();
+    x += 1;
+  }
+}
+export async function finalize(page) {
+  await page.waitForTimeout(3000);
+  await page.getByText('Continue').first().click();
+  await page.getByText('Finalize Reservation').first().click();
+}
