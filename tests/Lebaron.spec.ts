e2efadac4ab55eeeda11e8c9652ea627d9de7543
zach <perry.z@husky.neu.edu>
2025-07-17 10:57:07 -0400

    Refactored

--- tests/Lebaron.spec.ts
+++ tests/Lebaron.spec.ts
@@ -1,43 +1,39 @@
 import { test as setup, expect } from '@playwright/test';
 import 'dotenv/config';
-import { emptyDirectory, formatDate, removecarts, finalize, confirmOrRetry, whatday, formatDay, editbooking} from './aux-functions';
+import { emptyDirectory, formatDate, removecarts, finalize, confirmOrRetry, whatday, formatDay, editbooking } from './aux-functions';
 
-//Variables for the Boys
-declare var process : {
+//Set Up Secure Credential Referencing
+declare var process: {
   env: {
-      USERNAME: string;
-      PASSWORD: string;
-      URL: string;
+    USERNAME: string;
+    PASSWORD: string;
+    URL: string;
   }
 }
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
+
+//Tee Time Arrays
 const Tuesday_Times = ['12:30pm', '12:40pm']
 const WedThursFri_Times = ['2:30pm', '3:30pm']
 const Weekend_times = ['9:15am', '10:20am']
-
 
 // Get current date and create a new date that is 7 days ahead
 const currentDate = new Date();
 const oneWeekLater = new Date(currentDate);
 oneWeekLater.setDate(currentDate.getDate() + 7);
-
 var Day = formatDay(oneWeekLater)
 
+// Remove leading O on day
+if (Day[0] == '0') {
+  Day = Day[1]
+}
 
 // Print the formatted date of the current date and the new date (1 week later)
 console.log('Current Date:', formatDate(currentDate));
 console.log('One Week Later:', formatDate(oneWeekLater));
 console.log('One Week Later Day:', Day);
 console.log('Current Day of week:', whatday(currentDate));
-
-// If One week from now starts with a 0, remove it
-console.log('logme: ', Day[0])
-if (Day[0] == '0') {
-  console.log(Day[0])
-  Day = Day[1]
-}
-console.log(Day)
 
 ////////////////////////////////////////START OF FlOW//////////////////////////////////////////////////////
 setup('Check for Tee Times', async ({ page }) => {
@@ -50,7 +46,7 @@ setup('Check for Tee Times', async ({ page }) => {
   await page.locator('input[name="password"]').fill(password);
   await page.locator('button').filter({ hasText: 'Sign In' }).nth(1).click();
 
-  
+
   const DayofWeek = whatday(currentDate)
   console.log(DayofWeek)
 
@@ -65,7 +61,7 @@ setup('Check for Tee Times', async ({ page }) => {
     await page.getByText('Show more Mid Day tee times').first().click();
     await page.getByText(Tuesday_Times[0]).first().click();
     await confirmOrRetry(page, Tuesday_Times[1]);
-    await editbooking(page,1,9);
+    await editbooking(page, 1, 9);
     await removecarts(page);
     await finalize(page);
   }
@@ -75,7 +71,7 @@ setup('Check for Tee Times', async ({ page }) => {
     await page.getByText(Day, { exact: true }).click();
     await page.getByText(WedThursFri_Times[0]).first().click();
     await confirmOrRetry(page, WedThursFri_Times[1])
-    await editbooking(page,1,9);
+    await editbooking(page, 1, 9);
     await removecarts(page);
     await finalize(page);
   }
@@ -103,4 +99,3 @@ setup('Check for Tee Times', async ({ page }) => {
   await page.screenshot({ path: 'screenshots/Lebaron/tee_time.png' });
 
 });
-
